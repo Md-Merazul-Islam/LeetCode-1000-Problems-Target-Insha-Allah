1eb2c78bdfc46057032da37a71629c7a0208c49f
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-12-31 18:32:00 +0600

    implement solution for Reverse Nodes in k-Group

---
+++ Reverse Nodes in k-Group.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* start, ListNode* end) {
+        ListNode* prev = nullptr;
+        ListNode* cur = start;
+        ListNode* next = nullptr;
+
+        while (cur != end) {
+            next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1)
+            return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* preveG = &dummy;
+
+        while (true) {
+            ListNode* start = preveG->next;
+            ListNode* end = preveG;
+
+           
+            for (int i = 0; i < k && end; ++i)
+                end = end->next;
+
+            if (!end) break; 
+
+            ListNode* nextS = end->next;
+            ListNode* reveH = reverse(start, end->next);
+
+            preveG->next = reveH;
+            start->next = nextS;
+            preveG = start;
+        }
+        return dummy.next;
+    }
+};
