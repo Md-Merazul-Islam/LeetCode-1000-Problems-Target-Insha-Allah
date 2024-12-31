4ddae3f6bfa82ace889ca384e50ee46d4014616b
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-12-31 18:42:44 +0600

    implement solution for finding the index of the first occurrence in a string

--- Reverse Nodes in k-Group.cpp
+++ Reverse Nodes in k-Group.cpp
@@ -1,11 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    ListNode* reverse(ListNode* start, ListNode* end) {
-        ListNode* prev = nullptr;
-        ListNode* cur = start;
-        ListNode* next = nullptr;
+    ListNode *reverse(ListNode *start, ListNode *end)
+    {
+        ListNode *prev = nullptr;
+        ListNode *cur = start;
+        ListNode *next = nullptr;
 
-        while (cur != end) {
+        while (cur != end)
+        {
             next = cur->next;
             cur->next = prev;
             prev = cur;
@@ -14,31 +17,51 @@ public:
         return prev;
     }
 
-    ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
         if (!head || k == 1)
             return head;
 
         ListNode dummy(0);
         dummy.next = head;
-        ListNode* preveG = &dummy;
+        ListNode *preveG = &dummy;
 
-        while (true) {
-            ListNode* start = preveG->next;
-            ListNode* end = preveG;
+        while (true)
+        {
+            ListNode *start = preveG->next;
+            ListNode *end = preveG;
 
-           
             for (int i = 0; i < k && end; ++i)
                 end = end->next;
 
-            if (!end) break; 
+            if (!end)
+                break;
 
-            ListNode* nextS = end->next;
-            ListNode* reveH = reverse(start, end->next);
+            ListNode *nextS = end->next;
+            ListNode *reveH = reverse(start, end->next);
 
             preveG->next = reveH;
             start->next = nextS;
             preveG = start;
         }
         return dummy.next;
+    }
+};
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0)
+            return 0;
+        for (int i = 0; i <= n - m; i++)
+        {
+            if (haystack.substr(i, m) == needle)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };
