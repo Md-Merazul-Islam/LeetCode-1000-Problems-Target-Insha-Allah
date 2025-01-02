9ece35f2094bbe8569a6ae3e3d0b113155fba050
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-02 18:16:37 +0600

    feat: implement searchRange to find first and last position of target in sorted array

---
+++ Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = -1, end = -1;
+
+        // Find the first occurrence
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                start = mid;
+                r = mid - 1;  
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        // Find the last occurrence
+        l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+                l = mid + 1;  
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return {start, end};
+    }
+};
